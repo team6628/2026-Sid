4324959bdca50ed826d980f2a38527bbad73fc8a
team6628 <kmsbotkickers6628@gmail.com>
2026-02-17 16:02:56 -0600

    \

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,93 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class RobotContainer {
+
+    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
+
+    /* ROBOT CENTRIC DRIVE */
+    private final SwerveRequest.RobotCentric drive = new SwerveRequest.RobotCentric()
+            .withDeadband(MaxSpeed * 0.1)
+            .withRotationalDeadband(MaxAngularRate * 0.1)
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+    private final Telemetry logger = new Telemetry(MaxSpeed);
+
+    private final CommandJoystick joystick = new CommandJoystick(0);
+
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    private void configureBindings() {
+
+        drivetrain.setDefaultCommand(
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(-joystick.getY() * MaxSpeed)
+                     .withVelocityY(-joystick.getX() * MaxSpeed)
+                     .withRotationalRate(-joystick.getZ() * MaxAngularRate)
+            )
+        );
+
+        final var idle = new SwerveRequest.Idle();
+        RobotModeTriggers.disabled().whileTrue(
+            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
+        );
+
+        // SysId
+        joystick.button(7).and(joystick.button(4))
+            .whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+
+        joystick.button(7).and(joystick.button(3))
+            .whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+
+        joystick.button(8).and(joystick.button(4))
+            .whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+
+        joystick.button(8).and(joystick.button(3))
+            .whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+        // Reset heading (optional now, but keeping it)
+        joystick.button(5)
+            .onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+
+        drivetrain.registerTelemetry(logger::telemeterize);
+    }
+
+    public Command getAutonomousCommand() {
+
+        final var idle = new SwerveRequest.Idle();
+
+        return Commands.sequence(
+            drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
+
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(0.5)
+                     .withVelocityY(0)
+                     .withRotationalRate(0)
+            ).withTimeout(5.0),
+
+            drivetrain.applyRequest(() -> idle)
+        );
+    }
+}
