75d841e2d4734f0ea5687c8f89018d1e00d153b8
team6628 <kmsbotkickers6628@gmail.com>
2026-02-28 14:19:12 -0600

    fixed motors

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,18 +3,17 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Shooter extends SubsystemBase {
 
     private final SparkMax motorA;
     private final SparkMax motorB;
 
-    private static final double SHOOT_SPEED = 0.9;
+    private static final double SHOOT_SPEED = 0.8;
 
     public Shooter() {
-        motorA = new SparkMax(9, SparkLowLevel.MotorType.kBrushless); // PWM port 0
-        motorB = new SparkMax(10, SparkLowLevel.MotorType.kBrushless); // PWM port 1
+        motorA = new SparkMax(9, SparkLowLevel.MotorType.kBrushless);
+        motorB = new SparkMax(10, SparkLowLevel.MotorType.kBrushed);
 
         // Invert one motor so they spin opposite directions
         motorB.setInverted(true);
@@ -26,7 +25,7 @@ public class Shooter extends SubsystemBase {
     }
 
     public void outtake() {
-        motorA.set(-SHOOT_SPEED);
+        motorA.set(SHOOT_SPEED);
         motorB.set(-SHOOT_SPEED);
     }
 
